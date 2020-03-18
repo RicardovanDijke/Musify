bf4cd984cb27b82a982a5155ed80a80b52cc8c68
RicardovanDijke <ricardovandijke@gmail.com>
2020-03-18 16:10:21 +0100

    added laptop dev .bat file

---
+++ launch dev laptop.bat
@@ -0,0 +1,8 @@
+start "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" "C:\Users\ricar\Documents\repos\Musify\Backend\Musify Backend.sln"
+
+
+
+cd Musify-Web-App
+
+REM add npm install maybe
+start  cmd.exe /k ng serve --open

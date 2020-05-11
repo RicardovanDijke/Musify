b7769642a46b9a3ebb78e373c3946c3c52e7cfff
Ricardo van Dijke <Ricardovandijke@gmail.com>
2020-05-11 15:16:53 +0200

    added API Gateway using Ocelot

---
+++ Backend/API Gateway/Program.cs
@@ -0,0 +1,36 @@
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace API_Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, config) =>
+                    {
+                        config.AddJsonFile("configuration.json");
+                    }).ConfigureLogging((hostingContext, logging) =>
+                {
+                    //add your logging
+                    logging.AddConsole();
+                }).UseSerilog((_, config) =>
+                {
+                    config
+                        .MinimumLevel.Information()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .WriteTo.File(@"Logs\log.txt", rollingInterval: RollingInterval.Day);
+                }).UseStartup<Startup>();
+    }
+}
